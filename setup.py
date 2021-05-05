7ccbebff3be90d600ae52d6f4834792b61511d68
Yuji Suehiro <yuji.sue@gmail.com>
2021-05-05 09:55:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	name='YSCodes4Education',
 	version='0.1.0',
 	author='Yuji Suehiro',
-	packages=['test', 'test/test.py'],
+	packages=find_packages(),
 	package_dir={'': 'test'},
 	url='https://github.com/YujiSue/education',
 	description='Sample codes used for education.',
