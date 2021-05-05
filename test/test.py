92f30c137c744c88af7581ef554f828bd8443724
Yuji Suehiro <yuji.sue@gmail.com>
2021-05-05 09:15:13 +0900

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,16 @@
+class Test:
+  def __init__(self):
+    self.id = 0
+    self.name = 'Test'
+    
+  def print(self):
+    print(self.name)
+    
+  def getID(self):
+    return self.id
+  
+  def setID(self, i):
+    self.id = i
+    
+def test():
+    print("Hello!!!")
