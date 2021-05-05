f00c3f3187f18bce46dc84fe3acf5c1e1c339f84
Yuji Suehiro <yuji.sue@gmail.com>
2021-05-05 10:53:53 +0900

    Create ekarte.py

---
+++ ysedu/ekarte.py
@@ -0,0 +1,17 @@
+class 簡易版電子カルテ:
+  def __init__(self):
+    患者リスト = []
+    
+  def カルテの追加(self, ページ):
+    患者リスト.append(ページ)
+    
+  def 開く(self, ページ番号):
+    return 患者リスト[ページ番号]
+  
+class カルテページ:
+  def __init__(self):
+    self.患者ID = ''
+    self.患者氏名 = ''
+    
+  def 記入(self, ID, 氏名, 生年月日):
+    
