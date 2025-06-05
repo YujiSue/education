e1b2dc157354cd58830c5626c95a01cc0680c5b1
Yuji Suehiro <yuji.sue@gmail.com>
2025-06-06 07:59:56 +0900

    Add files via upload

---
+++ ysedu/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='ysedu',
+	version='1.1',
+	author='Yuji Suehiro',
+	packages=find_packages(),
+	url='https://github.com/YujiSue/education/ysedu',
+	description='Codes for education.'
+)
