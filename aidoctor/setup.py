3ffa7473ae0162deff92ab7e2be038f87ab41ade
Yuji Suehiro <yuji.sue@gmail.com>
2025-06-09 14:41:22 +0900

    Create setup.py

---
+++ aidoctor/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='ysaidoc',
+	version='1.1',
+	author='Yuji Suehiro',
+	packages=find_packages(),
+	url='https://github.com/YujiSue/education/aidoctor',
+	description='Sample codes for education.'
+)
